2c3182025544aa6e5c14310c817a0d51c5765452
Dustin Ketterman <ketterman.d@gmail.com>
2018-11-16 14:54:51 -0500

    Player Bar Buttons Assignment: Added Next Button functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -106,6 +106,14 @@ class Album extends Component {
 	// 	}
 	// }
 
+	handleNextClick(){
+		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+		const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
+		const newSong = this.state.album.songs[newIndex];
+		this.setSong(newSong);
+		this.play();
+	}
+
 	render(){
 		return (
 			<section className='album'>
@@ -138,6 +146,7 @@ class Album extends Component {
 					currentSong={this.state.currentSong}
 					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 					handlePrevClick={() => this.handlePrevClick()}
+					handleNextClick={() => this.handleNextClick()}
 				/>
 			</section>
 		)
