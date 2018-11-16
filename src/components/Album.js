db82d9809eded692b9232b088c35421ed58c760a
Dustin Ketterman <ketterman.d@gmail.com>
2018-11-16 17:20:57 -0500

    Player Bar Range Inputs Checkpoint - Added song time slider functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ class Album extends Component {
 			album: album,
 			currentSong: album.songs[0],
 			isPlaying: false,
+			currentTime: 0,
+			duration: album.songs[0].duration, // Can you just use currentSong.duration?
 			hoverVal: 0
 		};
 
@@ -21,6 +23,25 @@ class Album extends Component {
 		this.audioElement.src = album.songs[0].audioSrc;
 		this.playIcon = 'icon ion-md-play';
 		this.pauseIcon = 'icon ion-md-pause';
+	}
+
+	componentDidMount(){
+		this.eventListeners = {
+			timeupdate: e => {
+				this.setState({ currentTime: this.audioElement.currentTime });
+			}, 
+			durationchange: e => {
+				this.setState({ duration: this.audioElement.duration });
+			}
+		}; 
+		this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+		this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+	}
+
+	componentWillUnmount(){
+		this.audioElement.src = null;
+		this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+		this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
 	}
 
 	play(){
@@ -114,6 +135,12 @@ class Album extends Component {
 		this.play();
 	}
 
+	handleTimeChange(e){
+		const newTime = this.audioElement.duration * e.target.value;
+		this.audioElement.currentTime = newTime;
+		this.setState({ currentTime: newTime})
+	}
+
 	render(){
 		return (
 			<section className='album'>
@@ -144,9 +171,12 @@ class Album extends Component {
 				<PlayerBar
 					isPlaying={this.state.isPlaying}
 					currentSong={this.state.currentSong}
+					currentTime={this.audioElement.currentTime}
+					duration={this.audioElement.duration}
 					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 					handlePrevClick={() => this.handlePrevClick()}
 					handleNextClick={() => this.handleNextClick()}
+					handleTimeChange={(e) => this.handleTimeChange(e)}
 				/>
 			</section>
 		)
